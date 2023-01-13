2b314a2dbc6da4b0cdf06b401dc5efbf93bbd9ff
Marco Pellegrini <webdivision@taopatch.com>
2023-01-13 16:27:58 +0100

    add about page and work page

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require ("body-parser");
+const res = require("express/lib/response");
 
 const app = express();
 
 let items=["Buy food", "Cook food", "Eat food"];
+let workItems=[];
 
 
 app.set("view engine", "ejs");
@@ -11,7 +13,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("Public"));
 
-
+// template home
 app.get("/", function (req, res) {
   let today = new Date();
 
@@ -23,17 +25,38 @@ app.get("/", function (req, res) {
   
   let day=today.toLocaleDateString("it-IT", options);
 
-  res.render("list", { kindOfDay: day , NewListItems: items });
+  res.render("list", { listTitle: day , newListItems: items });
 });
 
-app.post("/", function(req,res) {
-  var item =req.body.newItem;
-  
-items.push(item);
+app.post("/", function(req,res){
+  let item = req.body.newItem;
 
-res.redirect("/");
+  if (req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect("/work");
+
+  } else{
+    items.push(item);
+    res.redirect("/");
+
+  }
+
 
 });
+
+
+
+//  template work
+app.get ("/work", function(req, res){
+  res.render("list", {listTitle: "Work list", newListItems: workItems});
+});
+
+app.get("/about", function (req,res){
+  res.render("about");
+})
+
+
+
 
 app.listen(3000, function () {
   console.log("server started on port 3000");
